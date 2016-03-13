0f6452584cfd7e432f7c99137f84340238255389
MoiseenkoGeorge <moiseenkogeorge1995@gmail.com>
2016-03-13 15:16:54 +0300

    add swap

--- EPAM.BSU.01.2016.Moiseenko.Task2/Algorithm.cs
+++ EPAM.BSU.01.2016.Moiseenko.Task2/Algorithm.cs
@@ -8,17 +8,18 @@ namespace EPAM.BSU._01._2016.Moiseenko.Task2
 {
     public static class Algorithm
     {
+        private static void Swap(int[] left,int[] right)
+        {
+            int[] temp = left;
+            left = right;
+            right = left;
+        }
         public static void BubbleSort(this int[][] array, Func<int[], int> operation)
         {
-            int[] temp = null;
             for (int i = array.Length - 1; i >= 0; i--)
                 for (int j = 0; j < i; j++)
                     if (operation(array[j]) > operation(array[j + 1]))
-                    {
-                        temp = array[j];
-                        array[j] = array[j + 1];
-                        array[j + 1] = temp;
-                    }
+                        Swap(array[j], array[j + 1]);
         }
     }
 }
